c6b9489ec6f122f7bcf6de695c7476e435efd95d
Rakesh <rakeshfisat@gmail.com>
2024-07-01 21:17:23 +0530

    demo page change added

--- src/main/java/com/qa/opencart/pages/ShoppingCartPage.java
+++ src/main/java/com/qa/opencart/pages/ShoppingCartPage.java
@@ -1,4 +1,19 @@
 package com.qa.opencart.pages;
 
+import com.qa.opencart.utils.ElementUtil;
+import org.openqa.selenium.WebDriver;
+
 public class ShoppingCartPage {
+
+    private WebDriver driver;
+    private ElementUtil elementUtil;
+
+    public ShoppingCartPage(WebDriver driver){
+        this.driver = driver;
+        System.out.println("Demo push 3");
+
+    }
+
+
+
 }
